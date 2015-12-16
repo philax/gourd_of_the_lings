e9b2ba6c61932a36d89260ff7ab3b753e50e9fae
Phil Laks <plaks@turbine.com>
2015-12-16 16:40:43 -0500

    first test

--- test_character.py
+++ test_character.py
@@ -1,10 +1,14 @@
 import unittest
-from Actor import *
+from character import *
 
-class TestPlayer(unittest.TestCase):
-	def __init__(self):
-		self.character = Character()
+class TestCharacter(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+		super(TestCharacter, self).__init__(*args, **kwargs)
+		self.character = Character(100, "test dude", 10)
 
-	def test_test(self)::
-		self.assertTrue(c.isathing())
-		c.blabla
+	def test_death(self):
+		self.assertTrue(self.character.is_alive())
+		self.character.hp = 0
+		self.assertFalse(self.character.is_alive())
+		self.character.hp = -1
+		self.assertFalse(self.character.is_alive())
